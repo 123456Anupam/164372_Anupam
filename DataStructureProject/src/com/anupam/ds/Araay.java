54ad15e5343653d868b808b6fc80d546eda63591
123456Anupam <44638379+123456Anupam@users.noreply.github.com>
2018-11-15 18:39:07 +0530

    Add files via upload

---
+++ DataStructureProject/src/com/anupam/ds/Araay.java
@@ -0,0 +1,16 @@
+package com.anupam.ds;
+
+public class Araay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner();
+		int empid=sc.nextInt();
+		String name=sc.nextLine();
+		int age=sc.nextLine();
+		MyArray e2=new MyArray();
+		e2.print();
+
+	}
+
+}
---
+++ DataStructureProject/src/com/anupam/ds/MyArray.java
@@ -0,0 +1,24 @@
+package com.anupam.ds;
+
+public class MyArray {
+   public static void main(String[] args)
+   {
+	   int eid;
+	   String ename;
+	   int eage;
+   }	
+	
+   void MyArray()
+   {
+	   eid=1;
+	   ename="Anupam";
+	   eage=21;
+   }
+   public void print()
+   {
+	   System.out.println(eid);
+	   System.out.println(ename);
+	   System.out.println(eage);
+   }
+   
+}
